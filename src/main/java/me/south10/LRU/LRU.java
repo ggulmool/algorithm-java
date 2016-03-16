067078d27d861d7f64c970a8ba764723c96f1d2b
south10 <south10@webcash.co.kr>
2016-03-16 16:21:52 +0900

    bubbleSort commit

---
+++ src/main/java/me/south10/LRU/LRU.java
@@ -0,0 +1,39 @@
+package me.south10.LRU;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+/**
+ * Created by iamssin on 2016-03-08.
+ */
+public class LRU<K,V> extends LinkedHashMap<K,V> {
+    private int maxSize;
+
+    public LRU(int size) {
+        super(size+1, 1, true);
+        this.maxSize = size;
+    }
+
+    @Override
+    protected boolean removeEldestEntry(Map.Entry<K, V> eldest) {
+        System.out.println("여기호출" + size());
+        return size() > maxSize;
+    }
+
+    public static void main(String[] args) {
+        LRU<String, Integer> lru = new LRU<String, Integer>(2);
+        lru.put("A",1);
+        System.out.println("A1");
+        lru.put("B",2);
+        System.out.println("B2");
+        lru.put("C",3);
+        System.out.println("C3");
+
+        System.out.println("lru.size() : " + lru.size());
+        if(lru.containsKey("A"))
+            System.out.println("LRU is failed");
+        else
+            System.out.println("Yes " + lru.size());
+
+    }
+}
---
+++ src/main/java/me/south10/sort/bubble/BubbleSort.java
@@ -0,0 +1,25 @@
+package me.south10.sort.bubble;
+
+/**
+ * Created by iamssin on 2016-03-16.
+ */
+public class BubbleSort {
+    public static void main(String[] args) {
+        int[] arr = {9, 6, 3, 5};
+
+        for(int i=0; i<arr.length-1; i++){
+            for(int j=0; j<arr.length-1-i; j++){
+                if(arr[j]>arr[j+1]){
+                    int tmp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = tmp;
+                }
+            }
+        }
+
+        for(int i=0; i<arr.length; i++){
+            System.out.print(arr[i]+" ");
+        }
+        System.out.println();
+    }
+}

8a22e62e36746bc1e8d3d4f40d016cc1ecb834e7
south10 <south10@webcash.co.kr>
2016-04-07 14:43:22 +0900

    pay2

---
+++ src/main/java/me/south10/solve/algorithm/chap01/Pay2.java
@@ -0,0 +1,18 @@
+package me.south10.solve.algorithm.chap01;
+
+/**
+ * Created by south10 on 2016-03-28.
+ */
+public class Pay2 {
+    public static void main(String[] args) {
+        int[] bills = {5, 2, 1};
+        int count = 0, money = 5, i0, i1;
+
+        for(i0 = money; i0 >= 0; i0-= bills[0])
+            for (i1 = i0; i1 >= 0; i1 -= bills[1])
+                if(i1 % bills[2] == 0)
+                    count++;
+
+        System.out.println("count : " + count);
+    }
+}
